e6d676ed046bf9422cff358f5e423a1179f81b6d
Ayush0639 <153727992+Ayush0639@users.noreply.github.com>
2024-06-25 15:49:38 +0530

    Update AiTrainerProject.py

--- posedetection_mediapipe/cvzone/AiTrainerProject.py
+++ posedetection_mediapipe/cvzone/AiTrainerProject.py
@@ -1,31 +1,8 @@
-"""import cv2
-import numpy as np
-import time
-import PoseModule as pm
-
-cap=cv2.VideoCapture("video.mp4")
-detector=pm.PoseDetector()
-
-while True:
-    #img=cv2.imread("test.jpg")
-    success,img=cap.read()
-    img=cv2.resize(img,(1280,720))
-
-    img=detector.findPose(img)
-    lmlist=detector.findPosition(img,False)
-    if len(lmlist)!=0:
-        detector.findAngle(img,12,14,16)
-        detector.findAngle(img,11,13,15)
-
-
-
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
-    """
 import cv2
 import PoseModule as pm
 
-cap = cv2.VideoCapture("video.mp4")
+# Initialize video capture
+cap = cv2.VideoCapture("cvzone/video.mp4")
 detector = pm.PoseDetector()
 
 while True:
@@ -33,20 +10,41 @@ while True:
     if not success:
         break
 
-    img = cv2.resize(img, (1280, 720))
-    img = detector.findPose(img)
-    lmList, _ = detector.findPosition(img, draw=False)
-    
-    if len(lmList) != 0:
-        angle1, img = detector.findAngle((lmList[12][0], lmList[12][1]),
-                                         (lmList[14][0], lmList[14][1]),
-                                         (lmList[16][0], lmList[16][1]),
-                                         img=img)
-        angle2, img = detector.findAngle((lmList[11][0], lmList[11][1]),
-                                         (lmList[13][0], lmList[13][1]),
-                                         (lmList[15][0], lmList[15][1]),
-                                         img=img)
+    img = cv2.resize(img, (1000, 700))  # Resize for better visibility
+    img = detector.findPose(img)  # Find the pose
+    lmList, _ = detector.findPosition(img, draw=False)  # Find landmarks without drawing
 
+    if len(lmList) != 0:
+        # Calculate angle for right arm (shoulder, elbow, wrist)
+        angle1, img = detector.findAngle((lmList[12][0], lmList[12][1]),
+                                          (lmList[14][0], lmList[14][1]),
+                                          (lmList[16][0], lmList[16][1]),
+                                          img=img)        # Calculate angle for left arm (shoulder, elbow, wrist)
+
+        angle2, img = detector.findAngle((lmList[11][0], lmList[11][1]),
+                                          (lmList[13][0], lmList[13][1]),
+                                          (lmList[15][0], lmList[15][1]),
+                                          img=img)
+        # Display angles on the image
+        cv2.putText(img, str(int(angle1)), 
+                    (lmList[14][1] - 50, lmList[14][2] + 50), 
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.putText(img, str(int(angle2)), 
+                    (lmList[13][1] - 50, lmList[13][2] + 50), 
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+
+        # Evaluate form based on angles
+        feedback = ""
+        if 182 < angle1 < 350 and 182 < angle2 < 350:
+            feedback = "Good form"
+        else:
+            feedback = "Correct your form"
+
+        # Display feedback on the image
+        cv2.putText(img, feedback, (50, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+
+    # Display the image
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
