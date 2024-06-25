48f992d0c4e0b1587d7730969232bab9dd37f8f0
Amogh-30 <152498994+Amogh-30@users.noreply.github.com>
2024-06-25 15:11:10 +0530

    Create AiTrainerProject.py

---
+++ AiTrainerProject.py
@@ -0,0 +1,55 @@
+"""import cv2
+import numpy as np
+import time
+import PoseModule as pm
+
+cap=cv2.VideoCapture("video.mp4")
+detector=pm.PoseDetector()
+
+while True:
+    #img=cv2.imread("test.jpg")
+    success,img=cap.read()
+    img=cv2.resize(img,(1280,720))
+
+    img=detector.findPose(img)
+    lmlist=detector.findPosition(img,False)
+    if len(lmlist)!=0:
+        detector.findAngle(img,12,14,16)
+        detector.findAngle(img,11,13,15)
+
+
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+    """
+import cv2
+import PoseModule as pm
+
+cap = cv2.VideoCapture("video.mp4")
+detector = pm.PoseDetector()
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    img = cv2.resize(img, (1280, 720))
+    img = detector.findPose(img)
+    lmList, _ = detector.findPosition(img, draw=False)
+    
+    if len(lmList) != 0:
+        angle1, img = detector.findAngle((lmList[12][0], lmList[12][1]),
+                                         (lmList[14][0], lmList[14][1]),
+                                         (lmList[16][0], lmList[16][1]),
+                                         img=img)
+        angle2, img = detector.findAngle((lmList[11][0], lmList[11][1]),
+                                         (lmList[13][0], lmList[13][1]),
+                                         (lmList[15][0], lmList[15][1]),
+                                         img=img)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
